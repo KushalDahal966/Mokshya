db727ba361ab483a9fbae802ba722c02f0116ef3
Kushal Dahal <kushaldahal761@gmail.com>
2025-08-06 20:39:21 +0545

    backup, want to try something

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -37,7 +37,7 @@ const config: Config = {
         },
         fontFamily: {
             primary: ['Poppins', 'sans-serif'],
-            secondary: ['Orelega One', 'cursive'],
+            secondary: ['Orelega One'],
         },
 
     },
