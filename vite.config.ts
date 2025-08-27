e1b50d522d953275d15ffaee89fffaca534b6f0c
Kushal Dahal <kushaldahal761@gmail.com>
2025-08-27 13:56:32 +0545

    again fixed some issue

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,24 @@
 import { defineConfig } from 'vite'
 import tailwindcss from '@tailwindcss/vite'
-export default defineConfig({  
-    plugins: [    
-        tailwindcss(),  
-    ],})
+import { resolve } from 'path'
+
+export default defineConfig({
+  plugins: [tailwindcss()],
+  server: {
+    // prevent Vite from always serving index.html
+    fs: {
+      allow: ['.'],
+    }
+  },
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        menu: resolve(__dirname, 'src/pages/menu.html'),
+        booking: resolve(__dirname, 'src/pages/booking.html'),
+        directions: resolve(__dirname, 'src/pages/directions.html'),
+        ktaHaru: resolve(__dirname, 'src/pages/ktaHaru.html'),
+      },
+    },
+  },
+})
